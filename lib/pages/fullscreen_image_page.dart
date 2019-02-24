88da350acc9873764ab11774261ce6597a4dbb51
Thomas Tuomenoksa <thotu84@gmail.com>
2019-02-24 16:04:54 +0100

    Fixed zooming images to work better, still needs some love

--- lib/pages/fullscreen_image_page.dart
+++ lib/pages/fullscreen_image_page.dart
@@ -1,4 +1,7 @@
 import 'dart:io';
+import 'dart:ui' as ui;
+import 'dart:math';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart';
 
@@ -16,65 +19,94 @@ class FullScreenImagePage extends StatefulWidget{
 
 class _FullScreenImagePageState extends State<FullScreenImagePage> {
 
+  double _maxScale = 20.0;
+  double _minScale = 1.0;
   double _scale = 1.0;
   double _previousScale = 1.0;
-  double _previousDX = 0.0;
-  double _previousDY = 0.0;
-  double _dx = 0.0;
-  double _dy = 0.0;
-  bool _hasPanned = false;
+
+  Offset _offset;
+  Offset _startingOffset;
+  Offset _initialOffsetDiff;
+
+  Image _image;
+
+  @override
+  void initState() {
+    _loadImage();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onScaleStart: (ScaleStartDetails details) {
-        _previousScale = _scale;
+    if (_image == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return GestureDetector(
+        onScaleStart: (ScaleStartDetails details) {
+          _previousScale = _scale;
 
-        if(!_hasPanned) {
-          _hasPanned = true;
-          _dx = details.focalPoint.dx;
-          _dy = details.focalPoint.dy;
-          _previousDX = details.focalPoint.dx;
-          _previousDY = details.focalPoint.dy;
-        } else {
-          _previousDX = _dx;
-          _previousDY = _dy;
-        }
-
-      },
-      onScaleUpdate: (ScaleUpdateDetails details){
-        setState(() {
-          if(_previousScale * details.scale < 1.0) {
-            _scale = 1.0;
-          } else {
-            _scale = _previousScale * details.scale;
+          if(_offset == null) {
+            _offset = details.focalPoint;
           }
 
-          //_dx = _previousDX - (details.focalPoint.dx -_previousDX);
-          //_dy = _previousDY + (_previousDY - details.focalPoint.dy);
-          _dx = details.focalPoint.dx;
-          _dy = details.focalPoint.dy;
+          // Focal point is in the middle when we're at minscale
+          if(_scale <= _minScale) {
+            _offset = details.focalPoint;
+          }
 
-        });
-      },
-      child: Container(
-        child: new Transform(
-          transform: new Matrix4.diagonal3(Vector3(_scale, _scale, _scale)),
-          origin: _getOffset(),
-          child: Image.file(File(widget.imagePath),
+          _startingOffset = details.focalPoint;
+          _initialOffsetDiff = _startingOffset - _offset;
+        },
+
+        onScaleUpdate: (ScaleUpdateDetails details) {
+          // Updated scale
+          double updatedScale = _previousScale * details.scale;
+          if(updatedScale >= _maxScale) {
+            updatedScale = _maxScale;
+          } else if(updatedScale <= _minScale) {
+            updatedScale = _minScale;
+          }
+
+
+          Offset updatedOffset = details.focalPoint - _initialOffsetDiff;
+
+
+          setState(() {
+            _scale = updatedScale;
+            _offset = updatedOffset;
+          });
+        },
+
+        onScaleEnd: (ScaleEndDetails details) {
+        },
+
+        child: Container(
+          child:
+          _offset == null
+              ?
+          Image.file(File(widget.imagePath))
+              :
+          new Transform(
+            transform: new Matrix4.diagonal3(
+                Vector3(_scale, _scale, _scale)),
+            origin: _offset,
+            child: Image.file(File(widget.imagePath)),
           ),
         ),
-      ),
-    );
-  }
-
-  Offset _getOffset() {
-    if(_dx == 0.0 && _dy == 0.0) {
-      return null;
-    } else {
-      return Offset(_dx, _dy);
-
+      );
     }
   }
 
+  static Image _loadImageFromPath(String imagePath) {
+    return Image.file(File(imagePath));
+  }
+
+  void _loadImage() async {
+    Image image = await _loadImageFromPath(widget.imagePath);
+    setState(() {
+      _image = image;
+    });
+  }
 }
